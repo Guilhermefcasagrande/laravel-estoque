1e014f9f8802c2b8c9514899f0049c3b8bb23007
Guilhermefcasagrande <guilhermefranciscocasagrande@gmail.com>
2019-03-03 11:19:48 -0300

    Criado seed de produto

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +10,27 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
+    public function run()    {
+        $this->call(ProdutoTableSeeder::class);
+    }
+}
+
+class ProdutoTableSeeder extends Seeder{
+    public function run(){
+        DB::insert("insert into produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)",
+            array("Geladeira", 2, 5900.00, "Com gelo na porta")
+        );
+        DB::insert("insert into produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)",
+            array("Fogão", 5, 950.00, "Painel digital")
+        );
+        DB::insert("insert into produtos
+            (nome, quantidade, valor, descricao)
+            values (?,?,?,?)",
+            array("Microondas", 1, 350.00, "Muito bom")
+        );
     }
 }
