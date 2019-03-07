dfb82803af58b97a3b32c43a0954bb25cf5e434f
Guilhermefcasagrande <guilhermefranciscocasagrande@gmail.com>
2019-03-07 20:22:05 -0300

    Trabalhando com Eloquent

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,30 +1,34 @@
 <?php namespace estoque\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
+use estoque\Produto;
 use Request;
 
 class ProdutoController extends Controller{
 
     public function lista(){
-        $produtos = DB::select("select * from produtos");
+        // $produtos = DB::select("select * from produtos");
+        $produtos = Produto::all();
 
         return view("produto/listagem")->with("produtos", $produtos);
     }
 
     public function listaJson(){
-        $produtos = DB::select("select * from produtos");
+        // $produtos = DB::select("select * from produtos");
+        $produtos = Produto::all();
 
         return response()->$produtos;
     }
 
     public function mostra(){
         $id = Request::route("id");
-        $produto = DB::select("select * from produtos where id = ?", [$id]);
+        // $produto = DB::select("select * from produtos where id = ?", [$id]);
+        $produto = Produto::find($id);
 
         if(empty($produto)){
             return "Esse produto não existe";
         }
-        return view("produto/detalhes")->with("p", $produto[0]);
+        return view("produto/detalhes")->with("p", $produto);
     }
 
     public function novo(){
@@ -32,14 +36,16 @@ class ProdutoController extends Controller{
     }
 
     public function adiciona(){
-        $nome = Request::input("nome");
+        $params = Request::all();
+        $produto = new Produto($params);
 
-        $nome = Request::input("nome");
-        $descricao = Request::input("descricao");
-        $valor = Request::input("valor");
-        $quantidade = Request::input("quantidade");
+        // $produto->nome = Request::input("nome");
+        // $produto->descricao = Request::input("descricao");
+        // $produto->valor = Request::input("valor");
+        // $produto->quantidade = Request::input("quantidade");
 
-        DB::insert("insert into produtos (nome, quantidade, valor, descricao) values (?, ?, ?, ?)", array($nome, $quantidade, $valor, $descricao));
+        // DB::insert("insert into produtos (nome, quantidade, valor, descricao) values (?, ?, ?, ?)", array($nome, $quantidade, $valor, $descricao));
+        $produto->save();
 
         // Retorna somente um valor do formulário preenchido
         return redirect()->action("ProdutoController@lista")->withInput(Request::only("nome"));
--- app/Produto.php
+++ app/Produto.php
@@ -5,5 +5,16 @@ namespace estoque;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model{
-    //Opcional para forçar o nome da tabela: protected $table = "produtos";
+    //Opcional para forçar o nome da tabela
+    protected $table = "produtos";
+    public $timestamps = false;
+
+    protected $fillable = array(
+        "nome",
+        "descricao",
+        "valor",
+        "quantidade"
+    );
+
+    protected $guarded = ["id"];
 }
